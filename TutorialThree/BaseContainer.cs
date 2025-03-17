f9e523ad8c52624c9c246a02edaae030d518eaac
Jasurbek Zokirov <s32257@pjwstk.edu.pl>
2025-03-17 16:25:56 +0100

    BaseContainer definition

---
+++ TutorialThree/BaseContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace TutorialThree;
+
+public class BaseContainer
+{
+    public float TotalMass { get; set; }
+    public float Height { get; set; }
+    public float Weight { get; set; }
+    public float Depth { get; set; }
+    public string SerialNumber { get; set; }
+    public float MaxPayload { get; set; }
+
+
+    public void EmptyCargo()
+    {
+        
+    }
+
+    public void LoadCargo()
+    {
+        
+    }
+}
---
+++ TutorialThree/ContainerType.cs
@@ -0,0 +1,6 @@
+﻿namespace TutorialThree;
+
+public enum ContainerType
+{
+    
+}
