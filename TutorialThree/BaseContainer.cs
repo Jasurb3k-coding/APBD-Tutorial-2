15f381bdac011cafaa1dcbe99acd64c5f1b76226
Jasurbek Zokirov <s32257@pjwstk.edu.pl>
2025-03-17 16:51:32 +0100

    Serial number, container type enum

--- TutorialThree/BaseContainer.cs
+++ TutorialThree/BaseContainer.cs
@@ -2,21 +2,33 @@
 
 public class BaseContainer
 {
-    public float TotalMass { get; set; }
+    public static int NumberOfExistingContainers = 0;
+    
+    public ContainerType ContainerType { get; set; }
+    public float CurrentTotalMass { get; set; }
     public float Height { get; set; }
     public float Weight { get; set; }
     public float Depth { get; set; }
-    public string SerialNumber { get; set; }
+    public string SerialNumber
+    { 
+        get
+        {
+            return $"KON-{ContainerType}-{++NumberOfExistingContainers}";
+        }
+    }
+
     public float MaxPayload { get; set; }
 
 
     public void EmptyCargo()
     {
-        
+        CurrentTotalMass = 0;
     }
 
-    public void LoadCargo()
+    public void LoadCargo(float massOfTheCargo)
     {
-        
+        if (massOfTheCargo < MaxPayload)
+            throw new OverfillException($"Cannot fit {MaxPayload}kgs into a container with max capacity of {MaxPayload}kgs");
+        CurrentTotalMass = massOfTheCargo;
     }
 }
--- TutorialThree/ContainerType.cs
+++ TutorialThree/ContainerType.cs
@@ -2,5 +2,7 @@
 
 public enum ContainerType
 {
-    
+    Liquid = 'L',
+    Gas = 'G',
+    Refrigerated = 'C',
 }
